e449e370501799151fe3faff7efa5581b9d46aaf
Jimmy B. <chips_xp@yahoo.com>
2024-07-10 00:50:17 -0400

    componenet additions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,45 +1,74 @@
-"use client";
+import CategoryFilter from "@/components/toptemp/CategoryFilter";
+import Collection from "@/components/toptemp/Collection";
+import Search from "@/components/toptemp/Search";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-// Home Splash Page (landing root) section.
-// Place bg-dotted-pattern image in className
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
+
   return (
     <>
-      <section className="bg-primary-50 bg-cover bg-center bg-fixed py-5 md:py10">
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="text-3xl font-bold">
-              Karaoke Events - Register, Promote, Organize
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
-              Create, Book, Email, and Attend your Karaoke Events
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
             </p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
-              <Link href="#events">Explore Events</Link>
+              <Link href="#events">Explore Now</Link>
             </Button>
           </div>
+
           <Image
-            src="/images/party-event.jpg"
-            alt="Party Events"
-            width={640}
-            height={470}
-            className="max-h[70vh] object-contain object-center 2xl:max-h[50vh]"
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
       </section>
-      <section id="events" className="wrapper flex flex-col my-8 gap-8">
-        <h2 className="h3-bold">
-          Trust By Hundreds of <br />
-          Events Managers and Promoters
+
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
         </h2>
+
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          <p>Search searchFilter</p>
+          <Search />
+          <CategoryFilter />
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
       </section>
     </>
   );
 }
-3;
